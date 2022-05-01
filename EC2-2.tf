2d3af047033038da51aed353ad1e8dfe2236bc8e
pmmanoj650 <100923361+pmmanoj650@users.noreply.github.com>
2022-05-01 18:53:38 +0530

    Update EC2-2.tf

--- EC2-2.tf
+++ EC2-2.tf
@@ -20,3 +20,8 @@ resource "aws_key_pair" "key" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFjDQaLcUYBJ6VFUlcp6Qtv1yavw5+jiOTdW5/iTmBgCXwH5j4b/VnD3NgfdtLlKT92HUkCakLJAABzd13gCe3ztsuIn5r3GDbCbNpCzpmL35WBHWXOuVonBK8kNGRq47e6KFBha7BI7YNakUTjdUSpCflrw6UibY89xpyQmKbIIhnXFb9a5RRYtK7WN3QiUItKXJ8lIgTH3OD6sALa6AeFOvqk6ukwvO4KzSmsNbasgbi94Hk3kuF+JFzJ3HRo8EoOb/1o/CfzhF5+bQiBDGfvj+9f+PJ+EPFCiHoiIx+Vuk7QybM0CwXvSdRicYorsp571Qqd8Go2aiDdfTO5TXBGZSX/zK7LsRICsl1iRaTpyRD32OC/gk2Rn5IHCkmaqwmRToJdWPHl3CSxChPpRXb5Kt4pwq9VY8rrvsJtUUOqmw95B1XR58E72kwkk8FyRZvr3HEMhfwVv6Ed7MkwcaXs8shEwuL3oPjJsbybQRqOJpJAHHvK8DS71iJj2LRaos= jyothsna@jyothsna-PC"
 }
 
+output "instance_public_ip" {
+  value = aws_instance.my-web.public_ip
+}
+
+
